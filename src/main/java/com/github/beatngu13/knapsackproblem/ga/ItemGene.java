a4e991801a29e0abc04095b0ae8407a0816520a6
Daniel Kraus <daniel.kraus@mailbox.org>
2019-07-14 00:56:46 +0200

    Add custom chromosome and gene

---
+++ src/main/java/com/github/beatngu13/knapsackproblem/ga/ItemGene.java
@@ -0,0 +1,38 @@
+package com.github.beatngu13.knapsackproblem.ga;
+
+import com.github.beatngu13.knapsackproblem.Problem;
+import com.github.beatngu13.knapsackproblem.base.Item;
+
+import io.jenetics.Gene;
+import io.jenetics.util.RandomRegistry;
+import lombok.Value;
+
+@Value
+public class ItemGene implements Gene<Item, ItemGene> {
+
+	private final Item item;
+
+	@Override
+	public boolean isValid() {
+		return true;
+	}
+
+	@Override
+	public Item getAllele() {
+		return item;
+	}
+
+	@Override
+	public ItemGene newInstance() {
+		final var random = RandomRegistry.getRandom();
+		final var index = random.nextInt(Problem.ITEMS.size());
+		final var item = Problem.ITEMS.get(index);
+		return newInstance(item);
+	}
+
+	@Override
+	public ItemGene newInstance(Item item) {
+		return new ItemGene(item);
+	}
+
+}
---
+++ src/main/java/com/github/beatngu13/knapsackproblem/ga/KnapsackChromosome.java
@@ -0,0 +1,56 @@
+package com.github.beatngu13.knapsackproblem.ga;
+
+import java.util.Iterator;
+
+import com.github.beatngu13.knapsackproblem.Problem;
+import com.github.beatngu13.knapsackproblem.base.Knapsack;
+
+import io.jenetics.Chromosome;
+import io.jenetics.util.ISeq;
+import lombok.Value;
+import lombok.val;
+
+@Value
+public class KnapsackChromosome implements Chromosome<ItemGene> {
+
+	private final Knapsack knapsack;
+
+	@Override
+	public boolean isValid() {
+		return knapsack.getWeight() <= Problem.MAX_CAPACITY;
+	}
+
+	@Override
+	public Iterator<ItemGene> iterator() {
+		return toSeq().iterator();
+	}
+
+	@Override
+	public Chromosome<ItemGene> newInstance() {
+		return new KnapsackChromosome(Knapsack.newInstance());
+	}
+
+	@Override
+	public Chromosome<ItemGene> newInstance(ISeq<ItemGene> genes) {
+		final val items = genes.map(ItemGene::getAllele).asList();
+		return new KnapsackChromosome(new Knapsack(items));
+	}
+
+	@Override
+	public ItemGene getGene(int index) {
+		return toSeq().get(index);
+	}
+
+	@Override
+	public int length() {
+		return knapsack.getItems().size();
+	}
+
+	@Override
+	public ISeq<ItemGene> toSeq() {
+		return knapsack.getItems().stream() //
+				.map(ItemGene::new) //
+				.collect(ISeq.toISeq());
+	}
+
+}
