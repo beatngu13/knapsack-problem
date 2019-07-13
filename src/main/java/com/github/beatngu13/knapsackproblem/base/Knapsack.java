193bc9bb3352db2f553e2368041d74b9d7b0ea82
Daniel Kraus <daniel.kraus@mailbox.org>
2019-07-14 00:56:31 +0200

    Add helper to create random knapsacks

--- src/main/java/com/github/beatngu13/knapsackproblem/base/Knapsack.java
+++ src/main/java/com/github/beatngu13/knapsackproblem/base/Knapsack.java
@@ -1,13 +1,27 @@
 package com.github.beatngu13.knapsackproblem.base;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.github.beatngu13.knapsackproblem.Problem;
+
+import io.jenetics.util.RandomRegistry;
 import lombok.Value;
 
 @Value
 public class Knapsack {
 
 	private List<Item> items;
+
+	public static Knapsack newInstance() {
+		final var random = RandomRegistry.getRandom();
+		final var items = Problem.ITEMS.stream() //
+				.filter(item -> random.nextBoolean()) //
+				.collect(Collectors.toList());
+		final var knapsack = new Knapsack(items);
+		// Make sure only valid knapsacks are created.
+		return knapsack.getWeight() <= Problem.MAX_CAPACITY ? knapsack : newInstance();
+	}
 
 	public int getProfit() {
 		return items.stream() //
