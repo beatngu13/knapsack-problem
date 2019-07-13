fce7c4bef478af91b931311b9dc6cee0995fd142
Daniel Kraus <daniel.kraus@mailbox.org>
2019-07-14 01:00:59 +0200

    Reuse weight getter

--- src/main/java/com/github/beatngu13/knapsackproblem/ga/WeightConstraint.java
+++ src/main/java/com/github/beatngu13/knapsackproblem/ga/WeightConstraint.java
@@ -1,7 +1,6 @@
 package com.github.beatngu13.knapsackproblem.ga;
 
 import com.github.beatngu13.knapsackproblem.Problem;
-import com.github.beatngu13.knapsackproblem.base.Item;
 
 import io.jenetics.Phenotype;
 import io.jenetics.engine.Constraint;
@@ -11,10 +10,7 @@ public class WeightConstraint implements Constraint<ItemGene, Integer> {
 	@Override
 	public boolean test(Phenotype<ItemGene, Integer> individual) {
 		final var knapsack = ((KnapsackChromosome) individual.getGenotype().getChromosome()).getKnapsack();
-		final var weight = knapsack.getItems().stream() //
-				.mapToInt(Item::getWeight) //
-				.sum();
-		return weight <= Problem.MAX_CAPACITY;
+		return knapsack.getWeight() <= Problem.MAX_CAPACITY;
 	}
 
 	@Override
