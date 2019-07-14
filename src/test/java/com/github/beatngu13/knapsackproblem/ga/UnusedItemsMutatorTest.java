53f784f2c8150f6002ee07e8483cec8cdf2c2130
Daniel Kraus <daniel.kraus@mailbox.org>
2019-07-14 23:56:56 +0200

    Add (failing) test for custom alterer

---
+++ src/test/java/com/github/beatngu13/knapsackproblem/ga/UnusedItemsMutatorTest.java
@@ -0,0 +1,77 @@
+package com.github.beatngu13.knapsackproblem.ga;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.github.beatngu13.knapsackproblem.Problem;
+import com.github.beatngu13.knapsackproblem.base.Item;
+import com.github.beatngu13.knapsackproblem.base.Knapsack;
+
+import io.jenetics.Genotype;
+import io.jenetics.Phenotype;
+import io.jenetics.util.Seq;
+
+class UnusedItemsMutatorTest {
+
+	@Test
+	void should_not_mutate_when_probability_is_zero() throws Exception {
+		final var knapsack = new Knapsack(Collections.emptySet());
+		final var cut = new UnusuedItemsMutator(0.0);
+
+		final var result = cut.alter(toPhenotypeSeq(knapsack), 1L);
+
+		final var knapsacks = toKnapsackList(result.getPopulation());
+		assertThat(knapsacks).size().isOne();
+		assertThat(knapsacks).first().isEqualTo(knapsack);
+	}
+
+	@Test
+	void should_not_mutate_knapsack_without_available_weight() throws Exception {
+		final var cut = new UnusuedItemsMutator(1.0);
+
+		final var result = cut.alter(toPhenotypeSeq(Problem.OPTIMAL_KNAPSACK), 1L);
+
+		final var knapsacks = toKnapsackList(result.getPopulation());
+		assertThat(knapsacks).size().isOne();
+		assertThat(knapsacks).first().isEqualTo(Problem.OPTIMAL_KNAPSACK);
+	}
+
+	@Test
+	void should_mutate_knapsack_with_available_weight() throws Exception {
+		final var knapsack = new Knapsack(Collections.emptySet());
+		final var cut = new UnusuedItemsMutator(1.0);
+
+		final var result = cut.alter(toPhenotypeSeq(knapsack), 1L);
+
+		final var knapsacks = toKnapsackList(result.getPopulation());
+		// Previously unused items, sorted by profit.
+		final var expected = new Knapsack(new HashSet<Item>(Problem.ITEMS.subList(9, Problem.ITEMS.size())));
+		assertThat(knapsacks).size().isOne();
+		assertThat(knapsacks).first().isEqualTo(expected);
+	}
+
+	private static Seq<Phenotype<ItemGene, Integer>> toPhenotypeSeq(final Knapsack... knapsacks) {
+		return Arrays.stream(knapsacks) //
+				.map(UnusedItemsMutatorTest::toPhenotype) //
+				.collect(Seq.toSeq());
+	}
+
+	private static Phenotype<ItemGene, Integer> toPhenotype(final Knapsack knapsack) {
+		return Phenotype.of(Genotype.of(new KnapsackChromosome(knapsack)), 1L);
+	}
+
+	private static List<Knapsack> toKnapsackList(final Seq<Phenotype<ItemGene, Integer>> population) {
+		return population.map(UnusedItemsMutatorTest::toKnapsack).asList();
+	}
+
+	private static Knapsack toKnapsack(final Phenotype<ItemGene, Integer> individual) {
+		return ((KnapsackChromosome) individual.getGenotype().getChromosome()).getKnapsack();
+	}
+
+}
