d6a4c2444361b7d09e2c37ff38af8ebe308a6261
Daniel Kraus <daniel.kraus@mailbox.org>
2019-07-15 18:40:19 +0200

    Add problem for MO knapsack

---
+++ src/main/java/com/github/beatngu13/knapsackproblem/mo/Problem.java
@@ -0,0 +1,34 @@
+package com.github.beatngu13.knapsackproblem.mo;
+
+import java.util.List;
+
+import com.github.beatngu13.knapsackproblem.base.Item;
+import com.github.beatngu13.knapsackproblem.base.Knapsack;
+import com.github.beatngu13.knapsackproblem.util.ProblemUtil;
+
+import lombok.experimental.UtilityClass;
+
+// Knapsack problem based on P06 from https://people.sc.fsu.edu/~jburkardt/datasets/knapsack_multiple/knapsack_multiple.html.
+@UtilityClass
+public class Problem {
+
+	public static final int MAX_CAPACITY_0 = 103;
+	public static final int MAX_CAPACITY_1 = 156;
+
+	private static final List<Integer> profits = List.of( //
+			78, 35, 89, 36, 94, //
+			75, 74, 79, 80, 16);
+
+	private static final List<Integer> weights = List.of( //
+			18, 9, 23, 20, 59, //
+			61, 70, 75, 76, 30);
+
+	public static final List<Item> ITEMS = ProblemUtil.getItems(profits, weights);
+
+	private static final String optimalSolution_0 = "1010010000";
+	private static final String optimalSolution_1 = "0001100010";
+
+	public static final Knapsack OPTIMAL_KNAPSACK_0 = ProblemUtil.getOptimalKnapsack(optimalSolution_0, ITEMS);
+	public static final Knapsack OPTIMAL_KNAPSACK_1 = ProblemUtil.getOptimalKnapsack(optimalSolution_1, ITEMS);
+
+}
