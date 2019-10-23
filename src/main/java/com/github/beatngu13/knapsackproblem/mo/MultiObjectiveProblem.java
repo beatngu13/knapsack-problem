fa1d6ea054b3c0abc498ae88930d1def3de84c51
Varun <varun.nayak@retest.de>
2019-10-23 19:00:06 +0200

    Update class to include max capacity and number of knapsacks

--- src/main/java/com/github/beatngu13/knapsackproblem/mo/MultiObjectiveProblem.java
+++ src/main/java/com/github/beatngu13/knapsackproblem/mo/MultiObjectiveProblem.java
@@ -14,21 +14,24 @@ public class MultiObjectiveProblem {
 
 	public static final int MAX_CAPACITY_0 = 103;
 	public static final int MAX_CAPACITY_1 = 156;
+	public static final int NUMBER_OF_KNAPSACKS = 2;
 
 	private static final List<Integer> profits = List.of( //
 			78, 35, 89, 36, 94, //
-			75, 74, 79, 80, 16);
+			75, 74, 79, 80, 16 );
 
 	private static final List<Integer> weights = List.of( //
 			18, 9, 23, 20, 59, //
-			61, 70, 75, 76, 30);
+			61, 70, 75, 76, 30 );
 
-	public static final List<Item> ITEMS = ProblemUtil.getItems(profits, weights);
+	public static final List<Item> ITEMS = ProblemUtil.getItems( profits, weights );
 
 	private static final String optimalSolution_0 = "1010010000";
 	private static final String optimalSolution_1 = "0001100010";
 
-	public static final Knapsack OPTIMAL_KNAPSACK_0 = ProblemUtil.getOptimalKnapsack(optimalSolution_0, ITEMS);
-	public static final Knapsack OPTIMAL_KNAPSACK_1 = ProblemUtil.getOptimalKnapsack(optimalSolution_1, ITEMS);
+	public static final Knapsack OPTIMAL_KNAPSACK_0 =
+			ProblemUtil.getOptimalKnapsack( optimalSolution_0, ITEMS, MAX_CAPACITY_0 );
+	public static final Knapsack OPTIMAL_KNAPSACK_1 =
+			ProblemUtil.getOptimalKnapsack( optimalSolution_1, ITEMS, MAX_CAPACITY_1 );
 
 }
